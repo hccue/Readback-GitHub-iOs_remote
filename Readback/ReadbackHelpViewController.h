3c55f783c6eb4bda34e93cb5def9e14ff40fe5e3
Santiago Borja <santiagoborja@CueBook.local>
2013-01-11 18:43:26 -0500

    V1.0 Help Page added with cmd speed link

---
+++ Readback/ReadbackHelpViewController.h
@@ -0,0 +1,17 @@
+//
+//  ReadbackHelpViewController.h
+//  Readback
+//
+//  Created by Santiago Borja on 1/11/13.
+//  Copyright (c) 2013 Santiago Borja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define PATTERN_VERSION @"Version %@"
+#define URL_FRIEND_APP @"https://itunes.apple.com/us/app/cmd-speed/id581569231?ls=1&mt=8"
+#define STRING_BUNDLE_VERSION @"CFBundleShortVersionString"
+
+@interface ReadbackHelpViewController : UIViewController
+
+@end
---
+++ Readback/ReadbackHelpViewController.m
@@ -0,0 +1,36 @@
+//
+//  ReadbackHelpViewController.m
+//  Readback
+//
+//  Created by Santiago Borja on 1/11/13.
+//  Copyright (c) 2013 Santiago Borja. All rights reserved.
+//
+
+#import "ReadbackHelpViewController.h"
+
+@interface ReadbackHelpViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *versionLabel;
+@end
+
+@implementation ReadbackHelpViewController
+@synthesize versionLabel = _versionLabel;
+
+- (IBAction)return:(UIButton *)sender {
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
+
+- (IBAction)visitFriendApp:(UIButton *)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:URL_FRIEND_APP]];
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    self.versionLabel.text = [NSString stringWithFormat:PATTERN_VERSION, [[NSBundle mainBundle] objectForInfoDictionaryKey:STRING_BUNDLE_VERSION ]];
+}
+
+- (void)viewDidUnload {
+    [self setVersionLabel:nil];
+    [super viewDidUnload];
+}
+
+@end
